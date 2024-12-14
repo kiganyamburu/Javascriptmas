4b35293847ba35584320c36ba4e012ab16e6a234
kiganyamburu <mburukiganya@gmail.com>
2024-12-14 16:33:30 +0300

    emojies added to the text

--- Day 3 - Santa's Emoji Hack/index.js
+++ Day 3 - Santa's Emoji Hack/index.js
@@ -26,11 +26,19 @@ Example input: ":cry:"
 Example output: ‍😄
 
 */ 
-function emojifyWord(word){
-    return;
+function emojifyWord(word) {
+    if (word.startsWith(":") && word.endsWith(":")) {
+        const shortcode = word.slice(1, -1);
+        if (hackedEmojis.hasOwnProperty(shortcode)) {
+            return hackedEmojis[shortcode];
+        }
+        
+    }
+    return word;
 }
 
 console.log(emojifyWord(":angry:"));
+console.log(emojifyWord(":thumbsdown:"));
 
 
 /* 2. Write a function to find any emoji shortcodes in a phrase.
@@ -40,14 +48,30 @@ Example input: "Just read your article :thumbsdown:"
 Example output: "Just read your article 👏"
 */ 
 
-function emojifyPhrase(phrase){
-    return;
+function emojifyPhrase(phrase) {
+    const word = phrase.split("");
+    const emojifiedWords = word.map(emojifyWord);
+    returnemojifiedWords.join(" ");
 }
 
+console.log(emojifyPhrase("just read your article: thumbsdown: "));
 console.log(emojifyPhrase("Those shoes :puke:"));
+
 
 
 // Stretch goal: don't just replace the shortcodes, but also 
 // any emojis are added directly to the text.
 
+function replaceDirectEmojis(phrase) {
+    const reverseEmojis = Object.fromEntries(
+        Object.entries(hackedEmojis).map(([key, value]) => [value, value])
+    );
 
+    return phrase
+        .split("")
+        .map((word => hackedEmojis[word]) || replaceDirectEmojis[word] || word)
+        .join("");
+    
+    
+    console.log(replaceDirectEmojis("those shoes");)
+}
