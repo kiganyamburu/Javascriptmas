f29431b7f31d94195b87a29647d0e2e78be93485
kiganyamburu <mburukiganya@gmail.com>
2024-12-16 18:22:16 +0300

    done

--- Day 6 - Snow globe Animation/index.js
+++ Day 6 - Snow globe Animation/index.js
@@ -1,12 +1,36 @@
 const snowGlobe = document.querySelector('.snow-globe')
 
 function createSnowflake() {
+    const snowflake = document.createElement('div')
+    snowflake.classList.add('snowflake')
+    snowflake.textContent = '❄️';
+
+
+    const startPosition = Math.random() * snowGlobe.offsetWidth;
+    snowflake.style.right = `${startPosition}px`;
+
+    const size = Math.random() * 10 + 10;
+    snowflake.style.fontSize = `${size}px`;
+
+    const duration = Math.random() * 3 + 2;
+    snowflake.style.animationDuration = `${duration}s`;
+
+    snowGlobe.appendChild(snowflake);
+
+    setTimeout(() => {
+        snowflake.remove();
+    }, duration * 1000);
+}
+
+
+
+
 /* 
 Challenge:
 1. Write JavaScript to create a snowflake and make it fall inside the snow globe. The snowflake should have a random starting position, animation duration, and size.
 2. See index.css
 */ 
-}
+
 
 setInterval(createSnowflake, 100) // Let's create a snowflake every 100 milliseconds!
 
